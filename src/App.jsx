bb0da9b4fb098f143c37bbd5c93acde0e25893b6
24kb1a05g5 <24kb1a05g5@nbkrist.org>
2026-09-25 12:16:15 +0530

    Added Counter App

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,37 @@
-function App() {
-  return (
-    <div>
-      <h1>Hello, I am learning React!</h1>
-      <p>This is my first React application.</p>
-      <button>Click Me</button>
-    </div>
-  )
-}
 
-export default App
+import React, { Component } from 'react';
+
+class Counter extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      count: 0
+    };
+
+    this.incrementCount = this.incrementCount.bind(this);
+  }
+
+  incrementCount() {
+    this.setState(prevState => ({
+      count: prevState.count + 1
+    }));
+  }
+
+  render() {
+    return (
+      <div style={{ textAlign: 'center', marginTop: '50px' }}>
+        <h1>Counter App</h1>
+
+        <p>
+          Current Count: <strong>{this.state.count}</strong>
+        </p>
+
+        <button onClick={this.incrementCount}>
+          Increment
+        </button>
+      </div>
+    );
+  }
+}
+export default Counter;
