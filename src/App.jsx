3f2edc343c6c63d12d7fa343faf5eddb574ccca9
24kb1a05g5 <24kb1a05g5@nbkrist.org>
2026-09-25 11:46:41 +0530

    Initial React project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1>Hello, I am learning React!</h1>
+      <p>This is my first React application.</p>
+      <button>Click Me</button>
+    </div>
+  )
+}
+
+export default App
